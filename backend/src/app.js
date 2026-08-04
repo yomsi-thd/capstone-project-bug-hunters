2c40160fd82a9c0c3cfbfe0bc42eb1e28162df0d
Hieu Ngo <s3820591@rmit.edu.vn>
2026-08-04 17:38:40 +0700

    update env example and app.js routes in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,9 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/project", projectRoutes);
+app.use("/api/auths", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/projects", projectRoutes);
 app.use("/api/classcoins", classCoinRoutes);
 
 app.get("/", (req, res) => {
