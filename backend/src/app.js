3b55e350537df190c694873aeed82404d8a357d2
Hieu Ngo <s3820591@rmit.edu.vn>
2026-08-06 15:04:50 +0700

    add Frontend_URL to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
